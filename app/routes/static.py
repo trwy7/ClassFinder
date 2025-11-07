f5e656f18d2aa5920c9d4f8db4dca755f00d36d0
trwy7 <47907719+trwy7@users.noreply.github.com>
2025-11-07 13:15:34 -0700

    Add the 3ds as a legacy client

--- app/routes/static.py
+++ app/routes/static.py
@@ -28,7 +28,7 @@ def index_cssf():
     ua = (request.headers.get("User-Agent") or "").lower()
     app.logger.debug(f"User-Agent: {ua}")
     m = re.search(r"os (\d+)[_.]", ua)
-    if m and any(x in ua for x in ("iphone", "ipad", "ipod", "cpu")) or "wiiu" in ua:
+    if m and any(x in ua for x in ("iphone", "ipad", "ipod", "cpu")) or "wiiu" in ua or '3ds' in ua:
         try:
             if (not m) or int(m.group(1)) <= 8:
                 return send_from_directory("static", "legacycss.css")
